e5fb754b5687e72e9652235bb1fa80627bb105ab
aligh <aghasizadeh70@gmail.com>
2012-06-27 16:51:41 +0430

    add rnd function and make randome colors

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ for (var i=0;i<20;i++){
 	document.write('<div class="box"></div>');
 }
 document.write('</div>')
+function rnd(a,b){
+	return Math.floor(Math.random()*(b-a+1))+a;
+}
+	
+function randColor(){
+	var c='#';
+	for(var i=0;i<6;i++){
+		c+=rnd(0,15).toString(16);
+	}
+	return c;
+}
+var boxes = document.getElementsByClassName('box');
